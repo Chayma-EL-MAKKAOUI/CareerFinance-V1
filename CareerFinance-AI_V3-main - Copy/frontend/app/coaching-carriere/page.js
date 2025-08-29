7f73fa83aa61d6158708908d0ddff4765462d8b5
Chayma-EL-MAKKAOUI <chayma.elmakkaoui.23@ump.ac.ma>
2025-08-29 07:36:07 +0100

    Delete CareerFinance-AI_V3-main - Copy directory

--- CareerFinance-AI_V3-main - Copy/frontend/app/coaching-carriere/page.js
+++
@@ -1,5 +0,0 @@
-import WorkingCareerCoaching from '../../components/CoachingCarriere/WorkingCareerCoaching';
-
-export default function CoachingCarrierePage() {
-  return <WorkingCareerCoaching />;
-}
--- CareerFinance-AI_V3-main - Copy/frontend/components/UI/FileUpload.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FileUpload = ({ onChange }) => {
-  return <input type="file" onChange={onChange} />;
-};
-
-export default FileUpload; 
